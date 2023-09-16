b7399698c876a10518e10ae951d3af8e69ebf286
Shivi Arora <shiviarora017@gmail.com>
2023-09-16 20:45:59 +0530

    chnging the home page

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask,request,render_template
+import numpy as np
+import pandas as pd
+
+from sklearn.preprocessing import StandardScaler
+from src.pipeline.predict_pipeline import CustomData,PredictPipeline
+
+application=Flask(__name__)
+
+app=application
+
+## Route for a home page
+
+ 
+
+@app.route('/',methods=['GET','POST'])
+def predict_datapoint():
+    if request.method=='GET':
+        return render_template('home.html')
+    else:
+        data=CustomData(
+            gender=request.form.get('gender'), # type: ignore
+            race_ethnicity=request.form.get('ethnicity'), # type: ignore
+            parental_level_of_education=request.form.get('parental_level_of_education'),
+            lunch=request.form.get('lunch'), # type: ignore
+            test_preparation_course=request.form.get('test_preparation_course'), # type: ignore
+            reading_score=float(request.form.get('writing_score')), # type: ignore
+            writing_score=float(request.form.get('reading_score')) # type: ignore
+
+        )
+        pred_df=data.get_data_as_data_frame()
+        print(pred_df)
+        print("Before Prediction")
+
+        predict_pipeline=PredictPipeline()
+        print("Mid Prediction")
+        results=predict_pipeline.predict(pred_df)
+        print("after Prediction")
+        return render_template('home.html',results=results[0])
+    
+
+if __name__=="__main__":
+    app.run(debug=True) 
--- src/utils.py
+++ src/utils.py
@@ -51,7 +51,7 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
         return report
 
     except Exception as e:
-        raise CustomException(e, sys)
+        raise CustomException(e, sys) # type: ignore
     
 def load_object(file_path):
     try:
@@ -59,4 +59,4 @@ def load_object(file_path):
             return pickle.load(file_obj)
 
     except Exception as e:
-        raise CustomException(e, sys)    
+        raise CustomException(e, sys)     # type: ignore
